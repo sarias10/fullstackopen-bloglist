7ba5fecf0c57794d7a37b58f2d2788a4f6e392d6
Sergio Arias <s-andres140@hotmail.com>
2024-05-26 18:38:38 -0500

    refactor models

--- app.js
+++ app.js
@@ -3,15 +3,9 @@ const config = require('./utils/config')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const Blog = require('./models/blog')
 
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
 
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = config.MONGODB_URI
 
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// El archivo index.js solo importa la aplicación real desde el archivo app.js y luego inicia la aplicación
+
 const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
