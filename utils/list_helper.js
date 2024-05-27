1bbcbc2be37f51ba5095b1519c907cab4424a468
Sergio Arias <s-andres140@hotmail.com>
2024-05-27 17:16:27 -0500

    4.5*: Helper Functions and Unit Tests, step 3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -7,7 +7,8 @@ const dummy = (blogs) => {
 const totalLikes = (blogs) => {
     /*La función devuelve la suma total de 
       me gusta en todas las publicaciones 
-      del blog*/
+      del blog
+      */
       const initialValue = 0;
       const sumWTotalLikes = blogs.reduce(
         (accumulator, currentValue) =>
@@ -20,7 +21,79 @@ const totalLikes = (blogs) => {
        
 }
 
+
+const blogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: "5a422ba71b54a676234d17fb",
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: "5a422bc61b54a676234d17fc",
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 2,
+      __v: 0
+    }  
+  ]
+
+const favoriteBlog = (blogs) => {
+
+    /*La función recibe una lista de blogs como parámetro. 
+    La función descubre qué blog tiene más 
+    Me gusta. Si hay muchos favoritos, basta con 
+    devolver uno de ellos.
+    */
+
+    //desestructuramos el objeto en sus propiedades y nos quedamos con el rest
+    const mayor = blogs.map(({ _id, url,__v, ...rest}) => (rest))
+    //el initialValue es {} (objeto vacio), max es el accumulator y blog es el currentValue
+    //si los likes del objeto max(accumulator) son mayores al del objeto likes(currentValue)
+    //entonces max queda con el mismo objeto, sino, max pasa a ser el currentValue
+    .reduce(
+        (max, blog) => max.likes > blog.likes ? max = max : max = blog, {}
+    )
+    
+    return mayor
+}
 module.exports ={
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
